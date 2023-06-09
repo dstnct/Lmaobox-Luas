e0e67ece7d34ddb65d973b1275cb1b6ef7a5dc65
Highkey <76597629+dstnct@users.noreply.github.com>
2023-06-10 01:46:01 +0300

    Updated
    reworked text style + position

--- KDCalc.lua
+++ KDCalc.lua
@@ -1,11 +1,14 @@
--- onDeath function credit: Derser (Habnone)
--- TODO: Make the Kill,Death,Assist and KD Counters reset to 0 when the player isn't in a server (in-game)
 
-local tahoma = draw.CreateFont( "Tahoma Bold", 17 , 200, 0x200, 0x010)
+-- onDeath function credit: Derser (Habnone)
+-- TODO: Make the Kill,Death,Assist and KD Counters reset to 0 when the player changed maps
+
+local tahoma = draw.CreateFont( "Tahoma Bold", 16 , 200, 0x200, 0x010)
 local killCount = 0;
 local deathCount = 0;
 local assistCount = 0;
 local KDCount = 0;
+
+previousIp = engine.GetServerIP()
 
 local function onDeath(event)
 
@@ -26,15 +29,30 @@ local function onDeath(event)
         end
     end
 end
--- if KD is greater than 2.2 kd text becomes yellow, if then it's greater than 3.3 it becomes red
+-- if KD is greater than 2 kd text becomes yellow, if then it's greater than 3 it becomes red
 local function textDraw()
     draw.SetFont(tahoma)
-    draw.Color( 255, 148, 112, 255 )
-    draw.Text( 20, 400, "Kills: " ..killCount)
-    draw.Color( 159, 90, 253, 255 )
-    draw.Text( 20, 420, "Deaths: " ..deathCount)
-    draw.Color( 3, 138, 255, 255 )
-    draw.Text( 20, 440, "Assists: " ..assistCount)
+    draw.Color( 255, 255, 255, 255 )
+    draw.Text( 20, 400, "Kills:")
+    
+    draw.Color(126, 227, 59, 255)
+    draw.Text( 80, 400, killCount)
+    
+    draw.Color( 255, 255, 255, 255 )
+    draw.Text( 20, 420, "Deaths:")
+   
+    draw.Color(126, 227, 59, 255)
+    draw.Text( 80, 420, deathCount)
+    
+    draw.Color( 255, 255, 255, 255 )
+    draw.Text( 20, 440, "Assists:")
+   
+    draw.Color(126, 227, 59, 255)
+    draw.Text( 80, 440, assistCount)
+    
+    draw.Color( 255, 255, 255, 255 )
+    draw.Text( 20, 460, "KD:")
+    
     --/////////////////////////////////////////////////////
     if(deathCount == 0) then
         KDCount = killCount
@@ -44,18 +62,30 @@ local function textDraw()
     
 
         draw.Color( 0, 230, 64, 255 )
-        draw.Text( 20, 460, "KD: " ..KDCount)
+        draw.Text( 80, 460, KDCount)
     
         if(KDCount >= 2) then
             draw.Color( 189, 155, 25, 255 )
-            draw.Text( 20, 460, "KD: " ..KDCount)
+            draw.Text( 80, 460, KDCount)
         end
         
         if(KDCount >= 3) then
             draw.Color( 175, 65, 84, 255 )
-            draw.Text( 20, 460, "KD: " ..KDCount)
+            draw.Text( 80, 460, KDCount)
         end
+
+        local currentIP = engine.GetServerIP()
+
+  
+
         
+        if (currentIP == nil or currentIP ~= previousIP) then
+            killCount = 0;
+            deathCount = 0;
+            assistCount = 0;
+            KDCount = 0;
+        end
+        previousIP = currentIP
 end
 
 callbacks.Register("FireGameEvent", "whenDeath", onDeath)
