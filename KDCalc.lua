d1600ba9151c5f2867c3e077c87a773ad3ec625c
Highkey <76597629+dstnct@users.noreply.github.com>
2023-06-13 13:11:13 +0300

    fixes + additions
    - fixed repeating floating points
    - KD calculator now resets when you switch servers / maps, disconnect / get kicked etc.

--- KDCalc.lua
+++ KDCalc.lua
@@ -7,8 +7,11 @@ local killCount = 0;
 local deathCount = 0;
 local assistCount = 0;
 local KDCount = 0;
+local str = KDCount
+
 
 previousIp = engine.GetServerIP()
+previousMap = engine.GetMapName()
 
 local function onDeath(event)
 
@@ -21,6 +24,9 @@ local function onDeath(event)
 
         if (localPlayer:GetIndex() == userid:GetIndex()) then
             deathCount = deathCount + 1
+            if(localPlayer:InCond(13) ) then
+                deathCount = deathCount - 1
+            end
         elseif (attacker:GetIndex() == localPlayer:GetIndex()) then
             killCount = killCount + 1
         elseif (assister:GetIndex() == localPlayer:GetIndex()) then
@@ -53,28 +59,30 @@ local function textDraw()
     draw.Color( 255, 255, 255, 255 )
     draw.Text( 20, 460, "KD:")
     
-    --/////////////////////////////////////////////////////
     if(deathCount == 0) then
         KDCount = killCount
+        str = killCount
     elseif(deathCount >=0) then
         KDCount = killCount / deathCount
+        str = killCount / deathCount
+        str = string.format("%.2f",KDCount) --thnx lnx
     end
-    
 
         draw.Color( 0, 230, 64, 255 )
-        draw.Text( 80, 460, KDCount)
+        draw.Text( 80, 460, str)
     
         if(KDCount >= 2) then
             draw.Color( 189, 155, 25, 255 )
-            draw.Text( 80, 460, KDCount)
+            draw.Text( 80, 460, str)
         end
         
         if(KDCount >= 3) then
             draw.Color( 175, 65, 84, 255 )
-            draw.Text( 80, 460, KDCount)
+            draw.Text( 80, 460, str)
         end
 
         local currentIP = engine.GetServerIP()
+        local currentMap = engine.GetMapName()
 
   
 
@@ -83,10 +91,18 @@ local function textDraw()
             killCount = 0;
             deathCount = 0;
             assistCount = 0;
-            KDCount = 0;
+            str = "0";
         end
+
+        if(currentMap ~= previousMap) then
+            killCount = 0;
+            deathCount = 0;
+            assistCount = 0;
+            str = "0";
+        end
+        previousMap = currentMap
         previousIP = currentIP
 end
-
+print(math.floor(KDCount+0.5))
 callbacks.Register("FireGameEvent", "whenDeath", onDeath)
 callbacks.Register( "Draw", textDraw)
